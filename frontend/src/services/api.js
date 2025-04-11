4d670f0ab0aa8cb22fe8785b4d80dc3fc6baf6b4
swarajmahadik123 <swarajmahadik1234@gmail.com>
2025-04-11 14:16:31 +0530

    api url updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 // api.js
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = `${
+  import.meta.env.VITE_API_URL || "http://localhost:5000/api"   
+}`;
 
 // Create axios instance with base configuration
 const api = axios.create({
